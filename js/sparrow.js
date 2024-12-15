e84a9ae105686ee7b526cba7f20a826ccc6d6eab
Sharon <102872908+sparrowchuchu@users.noreply.github.com>
2024-12-16 03:50:48 +0800

    index page

--- js/sparrow.js
+++ js/sparrow.js
@@ -22,58 +22,56 @@ setInterval(() => {
 let authorization  = cwaApi;
 let src = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001";  // 今明 36 小時天氣預報
 src += "?authorization=" + authorization;  // 加入參數 authorization
-
-async function getWeatherData(){
+async function getWeatherData(i){
     let response = await fetch(src);
     let data = await response.json();
     let location = data.records.location;
     let locElement = document.querySelector("#loc");
     let weatherElement = document.querySelectorAll(".cube");
     let cityElement = document.querySelector("#cities");
-    console.log(location[5].locationName);
-    locElement.innerHTML += location[5].locationName;  // 臺北市
-    console.log(weatherElement);
-    
-    weatherElement[0].innerHTML = "<h4>" + location[5].weatherElement[0].time[0].startTime + "</h4><h4>" + location[5].weatherElement[0].time[0].endTime + "</h4>" + 
-                                "<div>" + location[5].weatherElement[2].time[0].parameter.parameterName + "°C - " + location[5].weatherElement[4].time[0].parameter.parameterName + "°C</div>" +
-                                "<div>"+ location[5].weatherElement[0].time[0].parameter.parameterName +"</div>"+
-                                "<div>降雨機率 : " + location[5].weatherElement[1].time[0].parameter.parameterName + "%</div>"+
-                                "<div>" + location[5].weatherElement[3].time[0].parameter.parameterName + "</div>";
-    weatherElement[1].innerHTML = "<h4>" + location[5].weatherElement[0].time[1].startTime + "</h4><h4>" + location[5].weatherElement[0].time[1].endTime + "</h4>" + 
-                                "<div>" + location[5].weatherElement[2].time[1].parameter.parameterName + "°C - " + location[5].weatherElement[4].time[1].parameter.parameterName + "°C</div>" +
-                                "<div>"+ location[5].weatherElement[0].time[1].parameter.parameterName +"</div>"+
-                                "<div>降雨機率 : " + location[5].weatherElement[1].time[1].parameter.parameterName + "%</div>"+
-                                "<div>" + location[5].weatherElement[3].time[1].parameter.parameterName + "</div>";
-    weatherElement[2].innerHTML = "<h4>" + location[5].weatherElement[0].time[2].startTime + "</h4><h4>" + location[5].weatherElement[0].time[2].endTime + "</h4>" + 
-                                "<div>" + location[5].weatherElement[2].time[2].parameter.parameterName + "°C - " + location[5].weatherElement[4].time[2].parameter.parameterName + "°C</div>" +
-                                "<div>"+ location[5].weatherElement[0].time[2].parameter.parameterName +"</div>"+
-                                "<div>降雨機率 : " + location[5].weatherElement[1].time[2].parameter.parameterName + "%</div>"+
-                                "<div>" + location[5].weatherElement[3].time[2].parameter.parameterName + "</div>";
+    console.log(location);
+    console.log(location[i].locationName);
+    locElement.innerHTML = "";
+    locElement.innerHTML += location[i].locationName;  // 臺北市
+    cityElement.innerHTML = "";
+    weatherElement[0].innerHTML = "<h4>" + location[i].weatherElement[0].time[0].startTime + "</h4><h4>" + location[i].weatherElement[0].time[0].endTime + "</h4>" + 
+                                "<div>" + location[i].weatherElement[2].time[0].parameter.parameterName + "°C - " + location[i].weatherElement[4].time[0].parameter.parameterName + "°C</div>" +
+                                "<div>"+ location[i].weatherElement[0].time[0].parameter.parameterName +"</div>"+
+                                "<div>降雨機率 : " + location[i].weatherElement[1].time[0].parameter.parameterName + "%</div>"+
+                                "<div>" + location[i].weatherElement[3].time[0].parameter.parameterName + "</div>";
+    weatherElement[1].innerHTML = "<h4>" + location[i].weatherElement[0].time[1].startTime + "</h4><h4>" + location[i].weatherElement[0].time[1].endTime + "</h4>" + 
+                                "<div>" + location[i].weatherElement[2].time[1].parameter.parameterName + "°C - " + location[i].weatherElement[4].time[1].parameter.parameterName + "°C</div>" +
+                                "<div>"+ location[i].weatherElement[0].time[1].parameter.parameterName +"</div>"+
+                                "<div>降雨機率 : " + location[i].weatherElement[1].time[1].parameter.parameterName + "%</div>"+
+                                "<div>" + location[i].weatherElement[3].time[1].parameter.parameterName + "</div>";
+    weatherElement[2].innerHTML = "<h4>" + location[i].weatherElement[0].time[2].startTime + "</h4><h4>" + location[i].weatherElement[0].time[2].endTime + "</h4>" + 
+                                "<div>" + location[i].weatherElement[2].time[2].parameter.parameterName + "°C - " + location[i].weatherElement[4].time[2].parameter.parameterName + "°C</div>" +
+                                "<div>"+ location[i].weatherElement[0].time[2].parameter.parameterName +"</div>"+
+                                "<div>降雨機率 : " + location[i].weatherElement[1].time[2].parameter.parameterName + "%</div>"+
+                                "<div>" + location[i].weatherElement[3].time[2].parameter.parameterName + "</div>";
 
-
-    location[5].weatherElement.forEach((i) =>{
+    location[i].weatherElement.forEach((i) =>{
         console.log(i);
     })
-
-    // location.forEach(loc => {
-    //     city.innerHTML += "<div>" + loc.locationName + " : " + loc.weatherElement[2].time[0].parameter.parameterName + " ~ " + loc.weatherElement[4].time[0].parameter.parameterName + " °C" + "</div>"
-    // });
-    
 
     // 生成 Button Tag 
     location.forEach((loc,i) => {
         let special = ["臺北市", "高雄市","新北市", "臺中市", "臺南市", "桃園市"];
-        loc.innerHTML = "";
         if (special.includes(loc.locationName)){
-            cityElement.innerHTML += "<li class='btn-blue center' onclick=showData(" + i + ")>" + loc.locationName + "</li>"
+            cityElement.innerHTML += "<li class='btn-blue center' onclick=getWeatherData(" + i + ")>" + loc.locationName + "</li>"
         }else{
-            cityElement.innerHTML += "<li class='btn-blue center display-type' onclick=showData(" + i + ")>" + loc.locationName + "</li>"
+            cityElement.innerHTML += "<li class='btn-blue center display-type' onclick=getWeatherData(" + i + ")>" + loc.locationName + "</li>"
         }
     });
+    showLess();
 }
- 
-document.addEventListener('DOMContentLoaded', getWeatherData());
 
+document.addEventListener('DOMContentLoaded', getWeatherData(5));
+
+function showData(i){
+    console.log("showData ",i);
+    console.log(location[i].locationName);
+}
 
 /*  */
 function showMore(){
